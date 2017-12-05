65b2f50c4d659ab640c4daf4cad1bcb8ca8ac904
皋鸿 <yingbing.hyb@antfin.com>
2017-12-05 15:53:23 +0800

    add md

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 #wisdom
 
+## 快速开始
+* git clone https://github.com/hsjavaweb/wisdom.git
+* IDEA File->Open->/Users/hyberbin/jplus/wisdom/pom.xml Open as project
+* 运行JUNIT com.wisdom.modules.common.service.BuildData.rebuildDb 能通过即OK
 
+###下载jetty
+http://central.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.3.2.v20150730/jetty-distribution-9.3.2.v20150730.zip
+
+##验证启动
 http://localhost:8080/wisdom/api/merchant/listMerchantCombox
--- modules/wisdom-common/src/test/java/com/wisdom/modules/common/service/BuildData.java
+++ modules/wisdom-common/src/test/java/com/wisdom/modules/common/service/BuildData.java
@@ -3,6 +3,8 @@ package com.wisdom.modules.common.service;
 import java.io.FileReader;
 import java.nio.charset.Charset;
 
+import com.wisdom.framework.core.util.id.DefaultIdGenerator;
+import com.wisdom.framework.core.util.id.IdGenerator;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.FixMethodOrder;
@@ -27,7 +29,6 @@ import com.wisdom.modules.common.domain.MerchantsUsers;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
         "classpath*:test/applicationContext-test-conf.xml",
-        "classpath*:test/applicationContext-mq.xml",
         "classpath:com/wisdom/modules/common/applicationContext-dao.xml"
 })
 @FixMethodOrder(value = MethodSorters.NAME_ASCENDING)
@@ -46,7 +47,7 @@ public class BuildData {
     @Test
     public void insertMerchantsUsers() throws Exception {
         MerchantsUsers merchantsUsers = new MerchantsUsers();
-        merchantsUsers.setUserName("hyberbin");
+        merchantsUsers.setUserName(DefaultIdGenerator.defaultIdGenerator.getNextId());
         merchantsUsers.setEmail("hyberbin@qq.com");
         merchantsUsers.setAdminType(3);
         merchantsUsers.setRoleId(3);
@@ -56,37 +57,13 @@ public class BuildData {
         merchantsUsers.setDelFlag(0);
         merchantsUsers.setPassWord(SecureUtil.MD5String("{123456}hyberbin", "UTF-8"));
         merchantsUsersService.insert(merchantsUsers);
-        merchantsUsers.setUserName("harry");
-        merchantsUsers.setEmail("hyberbin@qq.com");
-        merchantsUsers.setPassWord(SecureUtil.MD5String("{123456}harry", "UTF-8"));
-        merchantsUsersService.insert(merchantsUsers);
-        merchantsUsers = new MerchantsUsers();
-        merchantsUsers.setUserName("hyberbin1");
-        merchantsUsers.setEmail("hyberbin@qq.com");
-        merchantsUsers.setAdminType(1);
-        merchantsUsers.setMercId("000001");
-        merchantsUsers.setIpLimit("*");
-        merchantsUsers.setLastLoginIp("127.0.0.1");
-        merchantsUsers.setDelFlag(0);
-        merchantsUsers.setPassWord(SecureUtil.MD5String("{123456}hyberbin1", "UTF-8"));
-        merchantsUsersService.insert(merchantsUsers);
-        merchantsUsers = new MerchantsUsers();
-        merchantsUsers.setEmail("hyberbin@qq.com");
-        merchantsUsers.setUserName("hyberbin2");
-        merchantsUsers.setAdminType(2);
-        merchantsUsers.setMercId("000001");
-        merchantsUsers.setIpLimit("*");
-        merchantsUsers.setLastLoginIp("127.0.0.1");
-        merchantsUsers.setDelFlag(0);
-        merchantsUsers.setPassWord(SecureUtil.MD5String("{123456}hyberbin2", "UTF-8"));
-        merchantsUsersService.insert(merchantsUsers);
     }
 
     @Test
     public void insertMerchant() throws BusinessException {
         Merchant merchant = new Merchant();
-        merchant.setLegalName("黄迎兵");
-        merchant.setMercId("000001");
+        merchant.setLegalName("张三");
+        merchant.setMercId(DefaultIdGenerator.defaultIdGenerator.getNextId());
         merchant.setMercKey("123456");
         merchant.setMercSafeKey("123456");
         merchant.setMobile("11213123131");
@@ -102,11 +79,6 @@ public class BuildData {
         merchant.setIpLimit("*");
         merchant.setHost("*");
         merchantService.insert(merchant);
-        merchant.setMercId("000000");
-        merchant.setMercName("系统商户");
-        merchant.setPayRate(0f);
-        merchant.setRemitRate(0f);
-        merchantService.insert(merchant);
     }
 
     @Test
@@ -121,7 +93,9 @@ public class BuildData {
         ScriptRunner runner = new ScriptRunner(dataSource.getConnection());
         Resources.setCharset(Charset.forName("GBK")); //设置字符集,不然中文乱码插入错误
         runner.setLogWriter(null);//设置是否输出日志
-        runner.runScript(new FileReader("/Users/hyberbin/jplus/wisdom/wisdom.sql"));
+        String basePath=BuildData.class.getResource("/").getPath();
+        basePath=basePath.substring(0,basePath.indexOf("modules"));
+        runner.runScript(new FileReader(basePath+"wisdom.sql"));
         runner.closeConnection();
         build();
     }
